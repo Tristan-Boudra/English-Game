0f8647fa785a0bd54ea798cf6f9d0221463441ff
Tristan-Boudra <boudratristan@gmail.com>
2022-11-24 11:07:53 +0100

    feat(index): merge two projects

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 (function(){
     function buildQuiz(){
-      // variable to store the HTML output
       const output = [];
   
-      // for each question...
       myQuestions.forEach(
         (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
           const answers = [];
-  
-          // and for each available answer...
           for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
             answers.push(
               `<label>
                     <input type="radio" name="question${questionNumber}" value="${letter}">
@@ -21,60 +13,53 @@
                 </label>`
             );
           }
-  
-          // add this question and its answers to the output
           output.push(
             `<div class="container-quizz-question">
                 <div class="container-quizz-question2">
-                    <div class="question"> ${currentQuestion.question} </div>
-                    <div class="answers"> ${answers.join('')} </div>
+                    <div class="question">${currentQuestion.question}</div>
+                    <div class="answers">${answers.join('')}</div>
                 </div>
             </div>`
           );
         }
       );
-  
-      // finally combine our output list into one string of HTML and put it on the page
       quizContainer.innerHTML = output.join('');
     }
   
     function showResults(){
-  
-      // gather answer containers from our quiz
       const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-      // keep track of user's answers
       let numCorrect = 0;
-  
-      // for each question...
       myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-        // find selected answer
         const answerContainer = answerContainers[questionNumber];
         const selector = `input[name=question${questionNumber}]:checked`;
         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
   
-        // if answer is correct
         if(userAnswer === currentQuestion.correctAnswer){
-          // add to the number of correct answers
           numCorrect++;
-  
-          // color the answers green
           answerContainers[questionNumber].style.color = '#5A8A6B';
         }
-        // if answer is wrong or blank
         else{
-          // color the answers red
           answerContainers[questionNumber].style.color = '#E66A74';
         }
       });
   
-      // show number of correct answers out of total
+      if(numCorrect < 5){
       resultsContainer.innerHTML = 
       `<div class="container-result">
-        ${numCorrect} out of ${myQuestions.length}
+        ${numCorrect} / ${myQuestions.length} 🥲
       </div>`;
-
+      } else if (numCorrect < 7){
+        resultsContainer.innerHTML = 
+        `<div class="container-result">
+          ${numCorrect} / ${myQuestions.length} 😃
+        </div>`;
+      }
+      else{
+        resultsContainer.innerHTML = 
+        `<div class="container-result">
+          ${numCorrect} / ${myQuestions.length} 😄
+        </div>`;
+      }
     }
   
     const quizContainer = document.getElementById('quiz');
@@ -139,9 +124,6 @@
       }
     ];
   
-    // Kick things off
     buildQuiz();
-  
-    // Event listeners
     submitButton.addEventListener('click', showResults);
   })();
