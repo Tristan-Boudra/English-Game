d942dda4928068717777b7ab597871060c7c02e4
Tristan-Boudra <boudratristan@gmail.com>
2022-11-24 12:05:21 +0100

    feat(script.js): add all questions for quizz

--- script.js
+++ script.js
@@ -67,61 +67,100 @@
     const submitButton = document.getElementById('submit');
     const myQuestions = [
       {
-        question: "1. Who invented JavaScript?",
+        question: "1. Who is the most ecological search engine between theses answers ? ",
         answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
+          a: "Google Chrome",
+          b: "Ecosia",
+          c: "Firefox",
+          d: "Opéra"
         },
-        correctAnswer: "c"
+        correctAnswer: "b"
       },
       {
-        question: "2. Which one of these is a JavaScript package manager?",
+        question: "2. Wich social network is ecologically comitted ?",
         answers: {
-          a: "Node.js",
-          b: "TypeScript",
-          c: "npm"
+          a: "Newmanity",
+          b: "Greenbook",
+          c: "Tgreen",
+          d: "Ecommunauté"
         },
-        correctAnswer: "c"
+        correctAnswer: "a"
       },
       {
-        question: "3. Which tool can you use to ensure code quality?",
+        question: "3. Who is the first ecological server ?",
         answers: {
-          a: "Angular",
-          b: "jQuery",
-          c: "RequireJS",
-          d: "ESLint"
+          a: "OxygenServ",
+          b: "PlanteServ",
+          c: "GreenServ",
+          d: "EcoServ"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "4. What is the triggering event for the start of green it ?",
+        answers: {
+          a: "The GIEC receive the nobel peace prize",
+          b: "the financial crises of 2008",
+          c: "WWF wins the nobel prize for the environment"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "5. What is the origin of qwant ?",
+        answers: {
+          a: "American",
+          b: "German",
+          c: "Swiss",
+          d: "French"
         },
         correctAnswer: "d"
       },
       {
-        question: "4. Who invented JavaScript?",
+        question: "6. What is the carbon footprint of an email with large attachments?",
         answers: {
-          a: "Douglas Crockford",
-          b: "Sheryl Sandberg",
-          c: "Brendan Eich"
+          a: "25",
+          b: "50",
+          c: "75",
+          d: "100"
         },
-        correctAnswer: "c"
+        correctAnswer: "b"
       },
       {
-        question: "5. Which one of these is a JavaScript package manager?",
+        question: "7. Do the wifi consumes more than the network ?",
         answers: {
-          a: "Node.js",
-          b: "TypeScript",
-          c: "npm"
+          a: "Yes",
+          b: "No"
         },
-        correctAnswer: "c"
+        correctAnswer: "b"
       },
       {
-        question: "6. Which tool can you use to ensure code quality?",
+        question: "8. Is downloading a film instead of streaming it is a good eco gesture ?",
         answers: {
-          a: "Angular",
-          b: "jQuery",
-          c: "RequireJS",
-          d: "ESLint"
+          a: "Yes",
+          b: "No"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "9. When do you have to change your phone ?",
+        answers: {
+          a: "2 year",
+          b: "3 years",
+          c: "4 years",
+          d: "When doesn't work"
         },
         correctAnswer: "d"
-      }
+      },
+      {
+        question: "10. How would you rate our serious game ?",
+        answers: {
+          a: "1-3",
+          b: "3-5",
+          c: "5-7",
+          d: "7-10"
+        },
+        correctAnswer: "d"
+      },
     ];
   
     buildQuiz();
